0ff70d89586d40d19af1e5a856778599fead4ee8
Samantha Burke <samanthaburke.uk@gmail.com>
2019-12-19 20:53:35 +0000

    playing with React

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Title extends React.Component {
+
+    render() {
+      return (
+          <div>
+            <h1>This is my title</h1>
+            <h2> This is my subtitle</h2>
+          </div> 
+      );
+    }
+  }
+  
+  
+  export default Title;
+  
