e02313bec64bb17fc57df8f315861239b953f975
Samantha Burke <samanthaburke.uk@gmail.com>
2020-01-22 15:30:50 +0000

    implements strikethrough on task when edit button is clicked

--- src/Tasks.js
+++ src/Tasks.js
@@ -2,41 +2,54 @@ import React from "react"
 
 class Tasks extends React.Component {
 
-    doneClicked= () => {
-        this.props.deleteTaskFunc(this.props.item.id)
-    }
+	doneClicked = () => {
+		this.props.deleteTaskFunc(this.props.item.id)
+	}
 
-    editClicked= () => {
-        this.props.editTaskFunc(this.props.item.id)
-    }
+	editClicked = () => {
+		this.props.editTaskFunc(this.props.item.id)
+	}
 
-    render() { 
-        return (
-            <div>
-                <div className="task-list">
-                    <div className="row">
-                        <div className="col-4">
-                            {this.props.item.description}
-                        </div>
-                        <div className="col-4">
-                            {this.props.item.category}
-                        </div>
-                        <div className="col-2">
-                            <button className="btn btn-info" onClick={this.editClicked}>
-                                Edit
-                            </button>
-                        </div>
-                        <div className="col-2">
-                            <button className="btn btn-success" onClick={this.doneClicked}>
-                                Done
-                            </button>
-                        </div>
-                    </div>
-                </div>
-        </div>
-        
-        )
-    }
+	render() {
+
+		let description;
+		let category;
+
+    if (this.props.item.completed) {
+			description = <div className="col-4 completedTask"> {this.props.item.description} </div>
+			category = <div className="col-4 completedTask"> {this.props.item.category}</div>
+			} 
+			else {
+				description = <div className="col-4"> {this.props.item.description} {this.props.item.category} </div>
+				category = <div className="col-4"> {this.props.item.category} </div>
+			}
+		
+		return (
+			<div>
+				<div className="task-list">
+					<div className="row">
+						<div className="col-4">
+							{description}
+						</div>
+						<div className="col-4">
+							{this.props.item.category}
+						</div>
+						<div className="col-2">
+							<button className="btn btn-info" onClick={this.editClicked}>
+								Edit
+              </button>
+						</div>
+						<div className="col-2">
+							<button className="btn btn-success" onClick={this.doneClicked}>
+								Done
+              </button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		)
+	}
 }
 
 export default Tasks;
