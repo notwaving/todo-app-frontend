b10ef144377b543499726fc02a1066b872842239
Samantha Burke <samanthaburke.uk@gmail.com>
2020-03-06 14:47:24 +0000

    taskCompleted now toggling on front end, App.js tidiied up

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
 
   state = {
     tasks: [
-      
+
     ]
   }
   // When component mounts, trigger this...
@@ -18,38 +18,38 @@ class App extends React.Component {
 
     // Fetch tasks from API
     axios.get('https://khp5u5qhka.execute-api.eu-west-2.amazonaws.com/dev/tasks/')
-    .then((response) => {
-      // Handle success
-      this.setState({
-        tasks: response.data.tasks
+      .then((response) => {
+        // Handle success
+        this.setState({
+          tasks: response.data.tasks
+        })
       })
-    })
-    .catch((error) => {
-      // Handle error
-      console.error(error);
-    });
+      .catch((error) => {
+        // Handle error
+        console.error(error);
+      });
   }
 
   deleteTask = (taskID) => {
     // Get the list of tasks from state
     const tasks = this.state.tasks;
-    
+
     // Sample DELETE request
     // https://khp5u5qhka.execute-api.eu-west-2.amazonaws.com/dev/tasks/07afe600-7d84-4502-9520-df2995dce0aa
     axios.delete(`https://khp5u5qhka.execute-api.eu-west-2.amazonaws.com/dev/tasks/${taskID}`)
-    .then((response) => {
-      // Identify the task that matches the given taskID and remove it
-      const updatedTasks = tasks.filter(item => item.taskID !== taskID);
-    // Update the state with the new collection of tasks (i.e. without the one we just removed)
-      this.setState({
-        tasks: updatedTasks
+      .then((response) => {
+        // Identify the task that matches the given taskID and remove it
+        const updatedTasks = tasks.filter(item => item.taskID !== taskID);
+        // Update the state with the new collection of tasks (i.e. without the one we just removed)
+        this.setState({
+          tasks: updatedTasks
+        });
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(error);
       });
-    })
-    .catch((error) => {
-      // Handle error
-      console.error(error);
-    });
-    
+
   }
 
   editTaskFunc = (taskID, taskToEdit) => {
@@ -58,11 +58,11 @@ class App extends React.Component {
         // Get the state of our tasks
         const currentTasks = this.state.tasks;
         // Loop through the tasks to find the correct matching object
-        for(let i = 0; i < currentTasks.length; i++) {
+        for (let i = 0; i < currentTasks.length; i++) {
           const task = currentTasks[i];
-          if(task.taskID === taskID) {
+          if (task.taskID === taskID) {
             // Once found a match, update the completed property
-            task.taskCompleted = 1;
+            task.taskCompleted = !task.taskCompleted;
             // Break out as no need to check anymore
             break;
           }
@@ -76,53 +76,8 @@ class App extends React.Component {
         // Handle error
         console.error(error);
       });
-    // Update state to reflect changes made to the task
+
   }
-
-  // editTaskFunc = (taskID, taskToEdit) => {
-
-  //   axios.put(`https://khp5u5qhka.execute-api.eu-west-2.amazonaws.com/dev/tasks/${taskID}`, taskToEdit)
-  //   .then((response) => {
-
-  //     // Get backeend to pass on taskID to frontend
-  //     taskToEdit.taskID = response.data.tasks.taskID;
-
-  //     // Go through array of tasks until you find the one selected
-  //     // const tasks = this.state.tasks;
-  //     // for(let i = 0; i < tasks.length; i++) {
-  //     //   const taskToEdit = tasks[i];
-  //       // Toggle taskCompleted true or false with a switch case
-  //       switch(taskToEdit.taskID === taskID){
-  //         case taskToEdit.taskCompleted === 0:
-  //           taskToEdit.taskCompleted = 1;
-  //           break;
-  //         case taskToEdit.taskCompleted === 1:
-  //           taskToEdit.taskCompleted = 0;
-  //           break;
-  //         default:
-  //           break;
-  //       }
-  //     //}
-      
-
-  //     //console.log(taskToEdit);
-  //     // Get the current list of tasks from state
-  //     const currentTasks = this.state.tasks;
-  //     // Add taskToAdd to the array
-  //     currentTasks.push(taskToEdit);
-
-  //     // Update the state
-  //     this.setState({
-  //     tasks: currentTasks
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     // Handle error
-  //     console.error(error);
-  //   });    
-  //   // Update state to reflect changes made to the task
-    
-  // }
 
   addTask = (taskDescription, taskCategory) => {
 
@@ -134,25 +89,25 @@ class App extends React.Component {
     };
 
     axios.post('https://khp5u5qhka.execute-api.eu-west-2.amazonaws.com/dev/tasks/', taskToAdd)
-    .then((response) => {
-      // Get backeend to pass on taskID to frontend
-      taskToAdd.taskID = response.data.tasks.taskID;
+      .then((response) => {
+        // Get backeend to pass on taskID to frontend
+        taskToAdd.taskID = response.data.tasks.taskID;
 
-      console.log(taskToAdd);
-      // Get the current list of tasks from state
-      const currentTasks = this.state.tasks;
-      // Add taskToAdd to the array
-      currentTasks.push(taskToAdd);
+        console.log(taskToAdd);
+        // Get the current list of tasks from state
+        const currentTasks = this.state.tasks;
+        // Add taskToAdd to the array
+        currentTasks.push(taskToAdd);
 
-      // Update the state
-      this.setState({
-      tasks: currentTasks
+        // Update the state
+        this.setState({
+          tasks: currentTasks
+        });
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(error);
       });
-    })
-    .catch((error) => {
-      // Handle error
-      console.error(error);
-    });
 
   };
 
@@ -163,10 +118,10 @@ class App extends React.Component {
         <MyForm addTaskFunc={this.addTask} />
         <TasksLeft tasksLeft={this.state.tasks.length} />
         <TaskListHeader />
-        <TaskList 
-          taskList={this.state.tasks} 
-          deleteTaskFunc={this.deleteTask} 
-          editTaskFunc={this.editTaskFunc} 
+        <TaskList
+          taskList={this.state.tasks}
+          deleteTaskFunc={this.deleteTask}
+          editTaskFunc={this.editTaskFunc}
         />
       </div>
 
